018a8364cfe745a2c76e1c8afd002c2b51f78b92
nicanor-alicaya <donalicaya@gmail.com>
2020-04-06 20:38:17 -0400

    initial commit

--- src/03_modules.py
+++ src/03_modules.py
@@ -13,10 +13,11 @@ import sys
 
 # Print out the OS platform you're using:
 # YOUR CODE HERE
+print(sys.platform)
 
 # Print out the version of Python you're using:
 # YOUR CODE HERE
-
+print(sys.version)
 
 import os
 # See the docs for the OS module: https://docs.python.org/3.7/library/os.html
